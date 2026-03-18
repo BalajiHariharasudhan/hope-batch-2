277e8f36770ad4d63546a195f064dc8dcc16e587
BalajiHariharasudhan <bala3507563@gmail.com>
2026-03-18 13:20:46 +0530

    Create 125. Valid Palindrome.java

---
+++ 125. Valid Palindrome.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s=s.toLowerCase();
+        s=s.replaceAll("[^A-Za-z0-9]","");
+        int front=0,rear=s.length()-1;
+        while(front<rear){
+            if(s.charAt(front)!=s.charAt(rear)){
+                return false;
+            }
+            front++;
+            rear--;
+        }
+        return true;
+
+    }
+}
